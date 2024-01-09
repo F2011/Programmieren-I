af7d8a0217d7e9a36d8e35ea50dbab168d02545a
Leon Fertig <leon.fertig@sap.com>
2024-01-09 11:41:14 +0100

    Ueberlauf, Zweierkomplement; Kaufmaennisches Runden (1)

---
+++ src/exercises/datatypes/shortValue.java
@@ -0,0 +1,10 @@
+package exercises.datatypes;
+
+public class shortValue {
+    public static void main(String[] args) {
+        short x = 32767;
+        System.out.println(x);
+        x++;
+        System.out.println(x);
+    }
+}
